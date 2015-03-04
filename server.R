60be53de46bfcadb1aa4cbddd03c86244f6b2176
ironholds <ironholds@gmail.com>
2015-03-03 23:59:21 -0500

    reorder dataset, finish @ohnorobo -inspired mapping

--- server.R
+++ server.R
@@ -3,45 +3,46 @@ library(ggplot2)
 library(RColorBrewer)
 library(scales)
 library(grid)
+library(maptools) #Mapping dependency
+library(rgeos) #Mapping dependency
+library(rworldmap) #SpatialPolygonDataFrame creation.
+library(RColorBrewer) #Colour scale definitions.
+library(gridExtra) #Multi-mapping
+library(mapproj) #mollweide projection
 
 #Plot for per-project data
-per_project_plot <- function(data){
-  data$pageviews_percentage <- data$pageviews_percentage/100
-  project_name <- unique(data$project)
-  language <- unique(data$language)
-  plot_value_extent <- round(max(data$pageviews_percentage),1)
-  if(plot_value_extent < max(data$pageviews_percentage)){
-    plot_value_extent <- plot_value_extent+0.1
+per_project_plot <- function(data, project){
+  names(data) <- c("country","count")
+  cdm <- joinCountryData2Map(data, joinCode="ISO2", nameJoinColumn="country", suggestForFailedCodes=TRUE)
+  missingCountries <- unique(cdm$ISO_A2[!(cdm$ISO_A2 %in% data$country)])
+  if(length(missingCountries) >= 1){
+    data <- rbind(data, data.frame(country=missingCountries, count=0))
   }
-    
+  cdm <- joinCountryData2Map(data, joinCode="ISO2", nameJoinColumn="country", suggestForFailedCodes=TRUE)
+  values <- as.data.frame(cdm[,c("count", "country")])
+  names(values) <- c("count", "id")
+  values <- unique(values)
+  fortified_polygons <- fortify(cdm, region = "country")
   palette <- brewer.pal("Greys", n=9)
   color.background = palette[2]
   color.grid.major = palette[3]
   color.axis.text = palette[6]
   color.axis.title = palette[7]
   color.title = palette[9]
-  ggplot(data, aes(reorder(country, pageviews_percentage), pageviews_percentage , fill = country)) +
-    geom_bar(stat = "identity") +
-  theme_bw(base_size=9) +
-    theme(panel.background=element_rect(fill=color.background, color=color.background)) +
-    theme(plot.background=element_rect(fill=color.background, color=color.background)) +
-    theme(panel.border=element_rect(color=color.background)) +
-    theme(panel.grid.major=element_line(color=color.grid.major,size=.25)) +
-    theme(panel.grid.minor=element_blank()) +
-    theme(axis.ticks=element_blank()) +
-    theme(legend.position = "none") +
-    theme(axis.text.x=element_text(size=10,color=color.axis.text)) +
-    theme(axis.text.y=element_text(size=10,color=color.axis.text)) +
-    theme(axis.title.x=element_text(size=12,color=color.axis.title, vjust=0)) +
-    theme(axis.title.y=element_text(size=12,color=color.axis.title, vjust=1.25)) +
+  ggplot(values) + 
+    geom_map(aes(fill = count, map_id = id),
+             map = fortified_polygons) +
+    expand_limits(x = fortified_polygons$long,
+                  y = fortified_polygons$lat) +
+    coord_equal() + 
+    coord_map(projection="mollweide") +
+    labs(title = paste(project, "pageviews, by country\n(log-scaled)"),
+         x = "", y = "") +
+    scale_fill_gradientn(colours=brewer.pal(9, "Blues")[3:8], name = "Percentage of pageviews\n(log-scaled)",
+                         trans = log10_trans()) +
     theme(plot.title = element_text(size=14, color = color.axis.title, face = "bold")) +
-    theme(plot.margin = unit(c(0.35, 0.2, 0.3, 0.35), "cm")) +
-  coord_flip() +
-  scale_y_continuous(labels = percent, breaks = c(0.01,seq(0.10,plot_value_extent,0.10)),
-                     limits = c(0,plot_value_extent)) +
-  labs(title = paste("Per-country pageviews for", project_name, paste0("(",language,")")),
-       y = "Percentage of pageviews",
-       x = "Country")
+    theme(plot.margin = unit(c(0, 0, 0, 0), "cm")) +
+    theme(axis.text=element_blank(), axis.ticks=element_blank())
 }
 
 #Load and format dataset
@@ -61,14 +62,14 @@ shinyServer(function(input, output) {
     }
   )
   output$country_distribution <- renderPlot({
-    per_project_plot(country_data[country_data$project == input$project,])
+    per_project_plot(country_data[country_data$project == input$project,c("country_iso","pageviews_percentage")], input$project)
   })
   output$project_output <- renderText(
     paste0("Data for ", input$project)
   )
   output$table <- renderDataTable(
     expr = {
-      cd <- country_data[country_data$project == input$project,]
+      cd <- country_data[country_data$project == input$project,c("country","language","project","pageviews_percentage")]
       cd[order(cd$pageviews_percentage, decreasing = TRUE),]
       },
     options = list(paging = FALSE, searching = FALSE)
